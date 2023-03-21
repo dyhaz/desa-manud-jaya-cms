fcb7de230fb0e9addc0139ec34ba4877ab6d0bd6
muhammad <m.dyhaz@gmail.com>
2023-03-21 17:48:25 +0700

    fix: fix logout function

--- src/app/layouts/topbar/topbar.component.ts
+++ src/app/layouts/topbar/topbar.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Output, EventEmitter, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
-import { AuthenticationService } from '../../core/services/auth.service';
+// import { AuthenticationService } from '../../core/services/auth.service';
 import { AuthfakeauthenticationService } from '../../core/services/authfake.service';
 import { environment } from '../../../environments/environment';
 import { CookieService } from 'ngx-cookie-service';
 import { LanguageService } from '../../core/services/language.service';
 import { TranslateService } from '@ngx-translate/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { AuthenticationService } from "@core/http/api";
 
 @Component({
   selector: 'app-topbar',
@@ -29,12 +30,14 @@ export class TopbarComponent implements OnInit {
   public loginName = '';
   public loginPhoto = '';
 
-  constructor(@Inject(DOCUMENT) private document: any, private router: Router, private authService: AuthenticationService,
+  constructor(@Inject(DOCUMENT) private document: any, private router: Router,
+              // private authService: AuthenticationService,
               private authFackservice: AuthfakeauthenticationService,
               public languageService: LanguageService,
               public translate: TranslateService,
               public _cookiesService: CookieService,
-              public domSanitizer: DomSanitizer) {
+              public domSanitizer: DomSanitizer,
+              private authenticationService: AuthenticationService) {
   }
 
   listLang = [
@@ -95,9 +98,11 @@ export class TopbarComponent implements OnInit {
   /**
    * Logout the user
    */
-  logout() {
+  async logout() {
     if (environment.defaultauth === 'firebase') {
-      this.authService.logout();
+      // this.authService.logout();
+    } else if (environment.defaultauth === 'api') {
+      await this.authenticationService.logout().toPromise();
     } else {
       this.authFackservice.logout();
     }
